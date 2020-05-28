025c8dd776a95faf365fc32c50592752de385c07
Wesley Sousa <wesleysilvadesousa@gmail.com>
2020-05-28 08:47:37 -0300

    Endpoint google custom search - Images Url

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -7,5 +7,6 @@ const RestaurantController = require('./controllers/RestaurantController');
 routes.get('/id', RestaurantController.getIdLocation);
 routes.get('/restaurantids', RestaurantController.getIdsRestaurant);
 routes.get('/detail', RestaurantController.getRestaurantDetail);
+routes.get('/images', RestaurantController.getImages);
 
 module.exports = routes;
